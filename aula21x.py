1bd544378af81c8ff5f15934dfa7cd132616d64f
aleciojuniorc-glitch <aleciojuniorc@gmail.com>
2026-09-21 09:40:09 -0300

    Add files via upload

---
+++ aula21x.py
@@ -0,0 +1,8 @@
+def media(a, b, casas=2):
+    m = (a + b) / 2
+    return round(m, casas)
+print(media(8, 6))
+# 7.0
+
+print(media(8, 5, 1))
+# 6.
---
+++ aula21xx.py
@@ -0,0 +1,7 @@
+def total(*precos, desc=0):
+    s = sum(precos)
+    return s * (1 - desc / 100)
+print(total(10, 25.5, 7))
+# 42.5
+print(total(10, 25.5, 7, desc=10))
+# 38.2
